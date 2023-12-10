aa36ff31dbad65ccad1109a355c4bf236db1fad6
MrFlinxy <mdimasn131@gmail.com>
2023-12-10 19:04:04 +0700

    Setting up TailwindCSS for flask templates

--- server_kimia_fisik/__init__.py
+++ server_kimia_fisik/__init__.py
@@ -4,7 +4,6 @@ from .routes import main
 
 def create_app():
     app = Flask(__name__)
-    app.config["DEBUG"] = True
     app.config["SECRET_KEY"] = "secret_key"
     app.register_blueprint(main)
     return app
