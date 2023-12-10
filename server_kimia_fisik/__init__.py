19df8b9e9da3a4236cf0bd21387af17b992b4d0c
MrFlinxy <mdimasn131@gmail.com>
2023-12-10 21:42:25 +0700

    Added load_dotenv to access secretkey from .env

--- server_kimia_fisik/__init__.py
+++ server_kimia_fisik/__init__.py
@@ -1,9 +1,13 @@
+from dotenv import load_dotenv
 from flask import Flask
+from os import environ
 from .routes import main
+
+load_dotenv("../.env")
 
 
 def create_app():
     app = Flask(__name__)
-    app.config["SECRET_KEY"] = "secret_key"
+    app.config["SECRET_KEY"] = environ.get("secretKey")
     app.register_blueprint(main)
     return app
